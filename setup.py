0bef7361f3e201befee1f26c8e1cc80b030d6d3d
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-16 11:38:36 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-static-root-finder',
     version='0.1',
     author='Robin',
-    author_email='robin@robinwinslow.co.uk',
+    author_email='robin.winslow@canonical.com',
+    url='https://github.com/ubuntudesign/django-static-root-finder',
     packages=[
         'django_static_root_finder'
     ],
     description=(
         'A static files finder to find files in the STATIC_ROOT directory.'
     ),
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "Django >= 1.3",
     ],
